2fe5429173846505f4460e90c5ba22e10a3dd3d6
affanabbas-786 <affan.abbas@marceltech.com>
2024-11-28 17:38:50 +0500

    CSV Page Done

--- web/frontend/pages/csvUpload.jsx
+++ web/frontend/pages/csvUpload.jsx
@@ -67,18 +67,32 @@ export default function CsvUpload() {
           </CardBody>
         </Card> */}
 
-        <div className="box h-6 align-middle d-flex borderorange shadow p-3 mt-5 bg-body-tertiary rounded">
+        <div className="box align-middle d-flex borderorange shadow p-3 mt-5 bg-body-tertiary rounded">
           <div className="row">
-            <div className="col box1 h-6 align-middle d-flex borderorange shadow p-3 m-2 bg-body-tertiary rounded  ">
-              <div className="uploadicon warning">
+            <div className="col align-items-center box1 h-6 align-middle d-flex borderorange shadow p-3 m-2 bg-body-tertiary rounded  ">
+              <div className="uploadicon">
                 <MdDriveFolderUpload />
+                <div className="uploadtext d-flex">
+                <h1>Drag and Drop the file you want</h1>
+                <h3>or</h3>
+                <Button className="btn-shine">UPLOAD FILE</Button>
+                </div>
+                <p className="text-mute">Support file types: AI, PSD, PDF</p>
               </div>
             </div>
-            <div className="col h-6 align-middle borderorange shadow p-3 m-2 bg-body-tertiary rounded ">
-              <Alert variant="warning">
-                <TiWarning />
-                This is a alert—check it out!
-              </Alert>
+            <div className="col h-6 align-middle p-3 m-2 bg-body-tertiary rounded ">
+              <div className="uploadcontent">
+                <Alert variant="warning">
+                  <TiWarning />
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Autem iusto suscipit quas temporibus unde dolore tenetur
+                    nesciunt dicta repellat labore! Rem ipsum ducimus aliquid!
+                    Dolor qui sit blanditiis quos nihil!
+                  </p>
+                  This is a alert—check it out!
+                </Alert>
+              </div>
             </div>
           </div>
         </div>
